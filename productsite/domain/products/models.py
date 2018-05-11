f407e08f8de00723cab7dc00e28de8828f875b34
Eric Trimble <ectrimble@hotmail.com>
2018-05-11 05:27:56 -0400

    Updated/Added database models for several domain entities, updated build script.  Build script now drops DB prior to build.

--- productsite/domain/products/models.py
+++ productsite/domain/products/models.py
@@ -9,6 +9,12 @@ cart = app_db.Table('cart',
                     app_db.Column('quantity', app_db.Integer, nullable=False, default=1)
                     )
 
+rating = app_db.Table('rating',
+                      app_db.Column('user_id', app_db.Integer, app_db.ForeignKey('user.id'), primary_key=True),
+                      app_db.Column('product_id', app_db.Integer, app_db.ForeignKey('product.id'), primary_key=True),
+                      app_db.Column('rating', app_db.Integer, nullable=False, default=3),
+                      )
+
 
 class Product(app_db.Model):
     id = app_db.Column(app_db.Integer, primary_key=True)
@@ -19,21 +25,16 @@ class Product(app_db.Model):
     expect_stock_quantity = app_db.Column(app_db.Integer, nullable=False, default=0)
     flag_out_of_stock = app_db.Column(app_db.Boolean, nullable=False, default=False)
     expect_restock_date = app_db.Column(app_db.DateTime, nullable=True)
-    # category_id = app_db.Column(app_db.Integer, app_db.ForeignKey('productcategory.id'), nullable=False)
-    # relationships: Images, Orders
-    # my plan is to have the orders be smart, if a customer
-    # selects buy 1 of N, N's inventory will be reduced by 1
-    # but the order will only be valid for X amount of time before
-    # the order is removed from the DB and the inventory and reset
+    category_id = app_db.Column(app_db.Integer, app_db.ForeignKey('product_category.id'), nullable=False)
+    category = app_db.relationship('Category', backref=app_db.backref('product', lazy=True))
 
     def __repr__(self):
         return "<Product('{}', '{}', '{}')>".format(self.id, self.title, self.quantity)
 
-"""
+
 class ProductCategory(app_db.Model):
     id = app_db.Column(app_db.Integer, primary_key=True)
     description = app_db.Column(app_db.String(45), unique=True, nullable=False)
 
     def __repr__(self):
         return "<ProductCategory('{}', '{}')>".format(self.id, self.description)
-"""
