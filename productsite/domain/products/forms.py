9a2c1efbc181228e071386de9c8d049ad622dd71
Eric Trimble <ectrimble@hotmail.com>
2018-05-10 22:42:21 -0400

    fixing Form import issue

--- productsite/domain/products/forms.py
+++ productsite/domain/products/forms.py
@@ -4,7 +4,7 @@ from wtforms import StringField, SubmitField, BooleanField, TextAreaField, Integ
 from wtforms.validators import DataRequired
 
 
-class ProductAdminAll(FlaskForm):
+class ProductAdminAddProduct(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
     quantity = IntegerField('Quantity', validators=[DataRequired()])
@@ -15,9 +15,12 @@ class ProductAdminAll(FlaskForm):
     submit = SubmitField('Create New Product')
 
 
-class ProductAdminAddProduct(FlaskForm, ProductAdminAll):
-    pass
-
-
-class ProductAdminUpdateProduct(FlaskForm, ProductAdminAll):
-    pass
+class ProductAdminUpdateProduct(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    quantity = IntegerField('Quantity', validators=[DataRequired()])
+    price = DecimalField('Price', validators=[DataRequired()])
+    expect_stock_quantity = IntegerField('Expected Stock Quantity', validators=[DataRequired()])
+    flag_out_of_stock = BooleanField('Out Of Stock')
+    expect_restock_date = DateField('Expected Restock Date')
+    submit = SubmitField('Create New Product')
